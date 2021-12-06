370fcdacf9edaff2f821f9c067398fb49e4cadad
Emanuele Buggin <emanuelebuggin@gmail.com>
2021-12-06 20:10:16 +0100

    Tolti dei commenti (prova)

--- CodeDayone.java
+++ CodeDayone.java
@@ -16,17 +16,9 @@ public class CodeDayone{
             j++;
             
         }
-        /*for(int i=0;i<dati.length;i++){
-            System.out.println(dati[i]);
-        }*/
-        in.close();
-        /*int contatore=0;
         
-        for(int i=0; i<dati.length-1;i++){
-            if(dati[i+1]>dati[i]){
-                contatore++;
-        }}
-        System.out.println(contatore);*/
+        in.close();
+
         int contatore = 0;
         for(int i=0; i<dati.length-3;i++){
             if((dati[i+1]+dati[i+2]+dati[i+3])>(dati[i]+dati[i+1]+dati[i+2])){
